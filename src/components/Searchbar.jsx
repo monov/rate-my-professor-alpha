af6f93e87275ff670aed9b308b5a2e8d9f08ef1e
monov <shohruhulous@gmail.com>
2023-11-11 16:19:48 +0500

    bug fixes

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -6,12 +6,15 @@ const Searchbar = () => {
     <div className="app-mainsection-search-wrapper">
       <div className="app-mainsection-search-input-wrapper">
         <input type="text" placeholder="Search by class..." />
-        <label className="switch">
+        <div className="switch-wrapper">
+          <label className="switch">
           <input type="checkbox" />
           <span className="slider round"></span>
           <span className="text class-text">class</span>
           <span className="text prof-text">prof.</span>
         </label>
+        </div>
+        
       </div>
       
     </div>
