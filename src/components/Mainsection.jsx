d8f456c7168a7aa4808657c024c2c56bf7e2d964
monov <shohruhulous@gmail.com>
2023-11-22 15:18:03 +0500

    fixed the switch

--- src/components/Mainsection.jsx
+++ src/components/Mainsection.jsx
@@ -5,26 +5,30 @@ import Infosection from "./Infosection";
 
 const Mainsection = ({ isPopup, setIsPopup }) => {
   const api = import.meta.env.VITE_API_BASE_URL;
-  const [profName, setProfName] = useState()
-  const [rate, setRate] = useState()
-  const [userName, setUserName] = useState()
-  const [comment, setComment] = useState()
+  const [profName, setProfName] = useState("Jakey Chan");
+  const [rate, setRate] = useState("5");
+  const [userName, setUserName] = useState("Jhonny Dang");
+  const [comment, setComment] = useState(
+    "I bought him chainz and got high score"
+  );
 
   useEffect(() => {
     lastComment();
   }, []);
   const lastComment = async () => {
-      const response = await fetch(`${api}reviews/last`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-      let data = await response.json()
-      setProfName(data.profId)
-      setRate(data.rating)
-      setUserName (data.userId)
-      setComment(data.reviewText)
+    const response = await fetch(`${api}reviews/last`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status == 200) {
+      let data = await response.json();
+      setProfName(data.profId);
+      setRate(data.rating);
+      setUserName(data.userId);
+      setComment(data.reviewText);
+    }
   };
   return (
     <div className="app-mainsection-main-wrapper">
@@ -48,16 +52,12 @@ const Mainsection = ({ isPopup, setIsPopup }) => {
               <div className="app-lastcomment-rate">{rate}</div>
             </div>
             <div className="app-main-last-comment-rest">
-              <div className="app-lastcomment-userName">
-                {userName}
-              </div>
+              <div className="app-lastcomment-userName">{userName}</div>
 
               <div className="app-lastcomment-profname">
                 Professor: <span>&nbsp;{profName}&nbsp;</span>
               </div>
-              <div className="app-lastcomment-comment">
-                {comment}
-              </div>
+              <div className="app-lastcomment-comment">{comment}</div>
             </div>
           </div>
         </div>
--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -4,12 +4,16 @@ import { redirect, useNavigate } from "react-router-dom";
 
 const Searchbar = () => {
   const [inputValue, setInputValue] = useState('');
+  const [slider, setSlider] = useState()
   const navigate = useNavigate()
 
   const handleInputChange = (event) => {
     setInputValue(event.target.value);
-      
   };
+  const handleSlider = ( ) => {
+    setSlider(v => !v)
+    console.log(slider)
+  }
   const handleRedirect = (e)=>{
     if(e.key == 'Enter'){
       console.log(e.key)
@@ -26,12 +30,12 @@ const Searchbar = () => {
         value={inputValue}
         placeholder="Search by class..." />
         <div className="switch-wrapper">
-          <label className="switch">
-          <input type="checkbox" />
-          <span className="slider round"></span>
-          <span className="text class-text">class</span>
-          <span className="text prof-text">prof.</span>
-        </label>
+          <div onClick={handleSlider} className={`switch ${slider ? "class" : "prof"}`}>
+            <div  className="slider"></div>
+            <span className={slider ? "none" : ""}>class</span>
+            <span className={slider ? "" : "none"}>prof.</span>
+          </div>
+        
         </div>
         
       </div>
